2eaccd31c8d5b00be3a9be50c56696d5673a32a8
Leviathes <73369223+Leviathes@users.noreply.github.com>
2022-10-19 09:58:31 -0400

    Add files via upload

---
+++ PDF_PRINTER/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+
+
+
+
+Process pdfProcess = new Process();
+string close = "close";
+
+
+
+
+string filename = new string("");
+while (filename != close)
+{
+    filename = Console.ReadLine();
+    if (filename == close) {return;}
+    pdfProcess.StartInfo.FileName = @"C:\Program Files (x86)\Foxit Software\Foxit Reader\FoxitPDFReader.exe";
+    pdfProcess.StartInfo.Arguments = string.Format((@"/t -p {0}"), ("\"../" + filename + ".pdf\""));
+    pdfProcess.Start();
+}
+
+
